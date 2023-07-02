941749fc816314e42dc65e52e042c5b39afc7ac4
NicolasGuruphat <nicolas.guruphat@gmail.com>
2023-07-02 21:25:34 +0200

    FEAT (ProgressBar) : display big break in yellow if the goal is achived

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,24 +1,28 @@
 <template>
     <template v-for="i in goal" :key="i">
-        <template v-if="i<=totalPomodoro">
+        <template v-if="i <= totalPomodoro">
             🟩
         </template>
-        <template v-else-if="i==totalPomodoro+1">
-            ⭕  
+        <template v-else-if="i == totalPomodoro + 1">
+            ⭕
         </template>
         <template v-else>
             🟥
         </template>
 
-        <template v-if="i%pomodoriByCycle==0">
-            🟨
+        <template v-if="i % pomodoriByCycle == 0">
+            <template v-if="goal <= totalPomodoro">
+                🟩
+            </template>
+            <template v-else>
+                🟨
+            </template>
         </template>
     </template>
-    
 </template>
 <script>
 export default {
-    props : {
+    props: {
         goal: Number,
         totalPomodoro: Number,
         pomodoriByCycle: Number

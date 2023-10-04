da2ee5bda795e2e86760b7d0a645424e1eee5526
NicolasGuruphat <nicolas.guruphat@gmail.com>
2023-10-04 13:40:55 +0200

    REFACTOR : use Vue Use fullscreen

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<template ref="app">
   <div class="progress-background-base" :style="progressBackgroundGradiant">
     <StatistiquesBlock class="stats-block" :currentPomodoroNumber="currentPomodoroNumber" :pomodoriByCycle="pomodoriByCycle"
       :totalPomodoriDone="totalPomodoriDone" :goal="goal" :timer="timer" :pomodoroTime="pomodoroTime" :breakTime="breakTime">
@@ -12,7 +12,8 @@
       @updateGradiantEnabled="($event: boolean) => grandiantEnabled = $event"
       @updateAudioEnabled="($event: boolean) => audioEnabled = $event">
     </OptionsBlock>
-    <img :style="[fullScreenActivated ? 'opacity:0.5' : 'opacity:1']" id="fullscreen-logo" @click="fullScreen()" :src="fullscreenLogo" alt="fullscreen-logo" />
+
+    <img :style="[isFullscreen ? 'opacity:0.5' : 'opacity:1']" id="fullscreen-logo"  @click="toggle" :src="fullscreenLogo" alt="fullscreen-logo" />
 
     <span id="timer" :class="{ 'working': working, 'not-working': !working }">{{ timer }}</span>
     <div>
@@ -43,21 +44,13 @@ import OptionsBlock from './components/OptionsBlock.vue'
 import ProgressBar from './components/ProgressBar.vue'
 import { ref, reactive, computed } from 'vue'
 import fullscreenLogo from '@/assets/fullscreen.svg'
+import { useFullscreen } from '@vueuse/core'
 // onMounted(() => {
 //   minutes.value = pomodoroTime.minutes
 //   seconds.value = pomodoroTime.seconds
 // })
-const fullScreenActivated = ref<boolean>(false)
-
-function fullScreen () : void {
-  if (document.fullscreenElement) {
-    document.exitFullscreen()
-    fullScreenActivated.value = false
-  } else {
-    document.documentElement.requestFullscreen()
-    fullScreenActivated.value = true
-  }
-}
+const app = ref(null)
+const { isFullscreen, toggle } = useFullscreen(app)
 
 const intervalId = ref<number | null>(null)
 function startTimer () : void {
